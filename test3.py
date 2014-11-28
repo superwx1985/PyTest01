b0dc3331533995642aeb0d5f8328485facdf4419
superwx1985 <superwx1985@gmail.com>
2014-11-28 12:33:23 +0800

    .

--- test3.py
+++ test3.py
@@ -8,9 +8,16 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import  Keys
 from selenium.webdriver.support.ui import Select
-import unittest,time,re
+import unittest, time, re
 import HTMLTestRunner
 
-list=[]
-if list == []:
-    raise Exception('aaaa')
+br = webdriver.Chrome()
+br.get('http://www.baidu.com')
+br.find_element_by_id('kw').send_keys('搜索一下')
+time.sleep(2)
+br.find_element_by_id('su').click()
+a=br.find_elements_by_css_selector('div.result.c-container a')
+
+for a1 in a:
+    print(a1.text)
+br.close()
