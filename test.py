27bc428b691b6bcb639f69c4fed4c52ab03c2a5f
vic wang <superwx1985@gmail.com>
2014-12-30 19:49:21 +0800

    research http API test

--- test.py
+++ test.py
@@ -1,5 +1,29 @@
 # -*- coding: utf-8 -*-
-import smtplib, time, os, mimetypes
-from email.mime.text import MIMEText
-from email.mime.multipart import MIMEMultipart
-from email.mime.image import MIMEImage
+import time, unittest, datetime, threading
+import import_test_data, wait_element
+from KWS import init
+from selenium import webdriver
+from KWS.test_case.public import KWS_module
+from KWS.my_test_case import MyTestCase
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common import exceptions 
+import httplib2
+
+
+
+url = 'http://qa.services.theknot.com/registry/v1/retailers/86132002'
+url_params = {'apikey':'ca7f6e91ee8134de9717707d86b29100','a':'1'}
+url=url+'?'
+for key, value in url_params.items():
+    url = url+key+'='+value+'&'
+
+h = httplib2.Http('.cache')
+response, content = h.request(url, 'GET')
+
+
+print(content)
+print(isinstance(content, str))
+str_content = content.decode('utf-8')
+
+print(str_content)
+print(isinstance(str_content, str))
--- test2.py
+++ test2.py
@@ -3,7 +3,7 @@
 import time, unittest, datetime, threading, wait_element
 from selenium import webdriver
 from KWS.test_case.public import KWS_module
-from KWS import import_test_data, init
+
 from KWS.my_test_case import MyTestCase
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
@@ -11,24 +11,5 @@ from selenium.webdriver.common.by import By
 from selenium.common import exceptions 
 
 
-def run_excel_tc(excel_file='D:/viwang/workspace/PyTest01/vic_test/test.xlsx', sheet='Sheet1'):
-    try:
-        #br = webdriver.Chrome()
-        #current_window = br.current_window_handle
-        data = import_test_data.get_excle_data(excel_file, sheet)
-        err = []
-        failed = []
-        assert (1==0) 
-        raise exceptions.WebDriverException('EEEE')
-    except AssertionError as err_:
-        failed.append(err_)
-    except exceptions.WebDriverException as err_:
-        err.append(err_)
-    except Exception as err_:
-        err.append(err_)
-    return err,failed
-    
-if __name__ == '__main__':
-    #unittest.main(warnings='ignore')
-    a=run_excel_tc()
-    print(a)
+data = dict(Id=86132002,Name='861320edit02',IsDeleted=False,CreatedBy='QA1',ModifiedBy='QA1')
+print (data)
