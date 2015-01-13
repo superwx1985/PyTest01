d9faa5da83277a00cbce5732458aac11820f6e59
vic wang <superwx1985@gmail.com>
2015-01-13 17:24:51 +0800

    .

--- test2.py
+++ test2.py
@@ -1,15 +1,6 @@
-# -*- coding: utf-8 -*-
-from selenium import webdriver
-def a():
-    err= []
-
-    try:
-        raise Exception('test')
-    except Exception as e:
-        err.append(e)
-        print(e)
-        raise
-    finally:
-        print(1)
-        
-a()
+a='hello world'
+print(a)
+a=1
+print(a)
+a=[1,'尼玛']
+print(a)
